5d98c63b6d425c80a6940b076001c24669229294
Maricar Walters <walterskai7@gmail.com>
2020-12-01 14:09:14 -0600

    initial commit for csharp lesson 2 studio, solved

---
+++ csharp-lesson2-studio/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace csharp_lesson2_studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> characterDictionary = new Dictionary<char, int>();
+
+            string str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit";
+
+            foreach (char character in str)
+            {
+                if (!characterDictionary.ContainsKey(character))
+                {
+                    characterDictionary.Add(character, 1);
+                }
+                else 
+                {
+                    characterDictionary[character]++;
+                } 
+            }
+
+            foreach (KeyValuePair<char,int> letter in characterDictionary)
+            {
+                Console.WriteLine(letter);
+            }
+        }
+    }
+}
